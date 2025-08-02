764344a2653cf591d28d6741e071c0f132963cce
kaspernj <k@spernj.org>
2025-08-02 15:31:02 +0000

    Fixed issues

--- src/use-press-outside.js
+++ src/use-press-outside.js
@@ -1,5 +1,5 @@
 import React from "react"
-import useContext from "./use-context"
+import useContext from "./use-press-outside-context"
 import {v4 as uuidv4} from "uuid"
 
 export default function usePressOutside(childRef, onPressOutside) {
